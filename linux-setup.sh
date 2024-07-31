616198982021e90949b3f395f5e2c557d40c2071
extreemedev <mattallev@gmail.com>
2024-08-01 01:18:24 +0200

    Rename setup.sh to linux-setup.sh and unset.sh to linux-unset.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Script di installazione per il progetto
+# Script to install packages for the project
 
-# Aggiorna i pacchetti e installa Node.js
+# Update packages and install Node.js
 echo "Aggiornamento pacchetti, in attesa di autorizzazione..."
 sudo apt-get update -y
+sudo apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev -y
+
+# Install the unnecessary packages for the project
+: '
 sudo apt install libnss3 -y
 sudo apt-get install libdbus-1-3 -y
 sudo apt-get install libatk1.0-0 -y
@@ -14,33 +18,35 @@ sudo apt-get install libdrm2 -y
 sudo apt-get install libgbm1 -y
 sudo apt-get install libasound2 -y
 sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev -y
+'
 
-
+# Installs nvm (Node Version Manager)
 echo "Installazione di Node.js..."
-# installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-# download and install Node.js (you may need to restart the terminal)
+# Download and install Node.js (you may need to restart the terminal)
 nvm install 20
 
-# verifies the right Node.js version is in the environment
-echo "Verifica versione di Node.js..."
+# Verifies the right Node.js version is in the environment
+echo "Verifying Node.js version..."
 node -v # should print `v20.16.0`
 
-# verifies the right npm version is in the environment
-echo "Verifica versione di npm..."
+# Verifies the right npm version is in the environment
+echo "Verifying npm version..."
 npm -v # should print `10.8.1`
 
-# Installa le dipendenze del progetto
-echo "Installazione delle dipendenze del progetto..."
+# Install project dependencies
+echo "Installing project dependencies..."
 
-echo "Installazione di Electron..."
+# Install Electron
+echo "Installing Electron..."
 npm install electron -D
 
-echo "Installazione di Bootstrap v5.3.3..."
+# Install Bootstrap v5.3.3
+echo "Installing Bootstrap v5.3.3..."
 npm install bootstrap@5.3.3
 
-echo "Installazione completata!"
+echo "Installation completed!"
--- linux-unset.sh
+++ linux-unset.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Script to remove packages for the project
+echo "Removing packages, waiting for authorization..."
+sudo apt remove libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev -y
 
-# Script di rimozione pacchetti per il progetto
-echo "Rimozione pacchetti, in attesa di autorizzazione..."
+# Remove the unnecessary packages for the project
+:'
 sudo apt remove libnss3 -y
 sudo apt remove libdbus-1-3 -y
 sudo apt remove libatk1.0-0 -y
@@ -12,17 +15,18 @@ sudo apt remove libdrm2 -y
 sudo apt remove libgbm1 -y
 sudo apt remove libasound2 -y
 sudo apt remove libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev -y
-
-
+'
+# Uninstall nvm (Node Version Manager)
 unset NVM_DIR
 
-# Rimuove Node.js
-echo "Rimozione di Node.js..."
+# Remove Node.js
+echo "Removing Node.js..."
 rm -rf ~/.nvm
 
-# Rimuove le dipendenze del progetto
-echo "Rimozione delle dipendenze del progetto..."
+# Remove project dependencies
+echo "Removing project dependencies..."
 rm -rf node_modules
 
-echo "Rimozione completata!"
+echo "Removal completed!"
+
 
